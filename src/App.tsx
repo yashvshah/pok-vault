ab8d2cad4217003076b0a96a0eeca4e0262185f3
yashvshah <shahyashvi55@gmail.com>
2026-01-12 16:52:03 +0530

    feat: implement routing with BrowserRouter and VaultPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Header from "./components/Header";
+import VaultPage from "./pages/valut";
+
 function App() {
   return (
-    <>
-      <div className="font-bold text-xl text-primary">heloo</div>
-    </>
+    <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Navigate to="/vault" />} />
+        <Route path="/vault" element={<VaultPage />} />
+        {/* <Route path="/markets" element={<MarketsPage />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
