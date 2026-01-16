f6735145cb14c59fc6eb1702b0dd5077b3a07525
yashnaman <shahyashs145@gmail.com>
2026-01-16 14:55:48 +0530

    fix: problem with walletconnect by not bundling walletconnect in vbite

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // this was needed to fix the wallet connect module not being properly opened
+  optimizeDeps: {
+    include: [
+      "@rainbow-me/rainbowkit",
+      "@walletconnect/modal",
+      "@walletconnect/ethereum-provider",
+      "wagmi",
+      "viem",
+    ],
+  },
 });
