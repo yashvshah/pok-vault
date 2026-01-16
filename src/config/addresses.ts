b1ec776171bfb8e44024f49bab7df3ebd41d6167
yashnaman <shahyashs145@gmail.com>
2026-01-16 21:28:16 +0530

    fix: update the bridge contract to be latest
    now that all of the old markets have been removed, we don't really care what happens to them

--- src/config/addresses.ts
+++ src/config/addresses.ts
@@ -4,16 +4,13 @@ export const EARLY_EXIT_FACTORY_ADDRESS = "0xe78d1d9f5b9daaccf6197a279698bb1e62d
 export const VAULT_OWNER_ADDRESS = "0x8A7f538B6f6Bdab69edD0E311aeDa9214bC5384A" as const;
 
 // ERC1155 Token addresses
-export const POLYGON_ERC1155_BRIDGED_BSC_ADDRESS = "0xB42D95Bd05713eD14369fC1a1e4fAF107b27c464" as const;
-//updated export const POLYGON_ERC1155_BRIDGED_BSC_ADDRESS = "0x77b0052A346B22Ea1F3112E3FCEF079567eD9979" as const
+export const POLYGON_ERC1155_BRIDGED_BSC_ADDRESS = "0x77b0052A346B22Ea1F3112E3FCEF079567eD9979" as const;
 export const POLYGON_ERC1155_POLYGON_ADDRESS = "0x4d97dcd97ec945f40cf65f87097ace5ea0476045" as const; 
 export const OPINION_ERC1155_ADDRESS = "0xAD1a38cEc043e70E83a3eC30443dB285ED10D774" as const;
 
 //Bride Addresses
 //send the Polymarket tokens to source bride address to bridge from Polygon to BSC
-export const POLYMARKET_SOURCE_BRIDGE_POLYGON_ADDRESS = "0xB42D95Bd05713eD14369fC1a1e4fAF107b27c464" as const;
-// updated export const POLYMARKET_SOURCE_BRIDGE_POLYGON_ADDRESS = "0x77b0052A346B22Ea1F3112E3FCEF079567eD9979" as const;
-// use these after removing the outcome token pairs from the old bridged token contracts
+export const POLYMARKET_SOURCE_BRIDGE_POLYGON_ADDRESS = "0x77b0052A346B22Ea1F3112E3FCEF079567eD9979" as const;
 
 // send the Polymarket tokens to destination bridge address to bridge from BSC to Polygon
 export const POLYMARKET_DESTINATION_BRIDGE_BSC_ADDRESS = POLYGON_ERC1155_BRIDGED_BSC_ADDRESS;
